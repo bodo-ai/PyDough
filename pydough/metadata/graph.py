a58bf546a4775fc95c676944e7aabc8a512417aa
knassre-bodo <kian@bodo.ai>
2024-10-14 00:35:05 -0400

    Setting up more of the pre-verification steps for metadata parsing

--- pydough/metadata/graph.py
+++ pydough/metadata/graph.py
@@ -1,6 +1,11 @@
 from typing import List, Union, Dict, Tuple
 from collections import defaultdict
 
+class PyDoughMetadataException(Exception):
+    """
+    TODO: add class docstring
+    """
+
 class PropertyMetadata(object):
     """
     TODO: add class docstring
@@ -15,29 +20,73 @@ class CollectionMetadata(object):
     def __init__(self, name: str):
         self.name = name
 
+    def verify_metadata(graph_name: str, collection_name: str, collection_json: Dict) -> None:
+        """
+        TODO: add function doscstring.
+        """
+        error_name = f"collection {repr(collection_name)} in graph {repr(graph_name)}"
+        if "properties" not in collection_json:
+            raise PyDoughMetadataException(f"Metadata for {error_name} missing required property 'properties'.")
+        if not isinstance(collection_json["properties"], dict):
+            raise PyDoughMetadataException(f"Property 'properties' of PyDough {error_name} must be a JSON object.")
+
     def get_nouns(self) -> List[Tuple[str, Union["CollectionMetadata", PropertyMetadata]]]:
+        """
+        TODO: add function doscstring.
+        """
         nouns = [(self.name, self)]
         return nouns
 
     def get_property_names(self) -> List[str]:
+        """
+        TODO: add function doscstring.
+        """
         return [property.name for property in self.properties]
 
     def get_property(self) -> List[str]:
+        """
+        TODO: add function doscstring.
+        """
         return [property.name for property in self.properties]
     
-    def solidify_properties(self, graph: "GraphMetadata") -> None:
-        return
+class SimpleTableMetadata(CollectionMetadata):
+    """
+    TODO: add class docstring
+    """
+    
+    def verify_metadata(graph_name: str, collection_name: str, collection_json: Dict) -> None:
+        """
+        TODO: add function doscstring.
+        """
+        CollectionMetadata.verify_metadata(graph_name, collection_name, collection_json)
+        
+        error_name = f"simple table collection {repr(collection_name)} in graph {repr(graph_name)}"
+        
+        if "table_path" not in collection_json:
+            raise PyDoughMetadataException(f"Metadata for {error_name} missing required property 'table_path'.")
+        if not isinstance(collection_json["table_path"], str):
+            raise PyDoughMetadataException(f"Property 'table_path' of {error_name} must be a string.")
+        
+        if "unique_properties" not in collection_json:
+            raise PyDoughMetadataException(f"Metadata for {error_name} missing required property 'table_path'.")
+        if not isinstance(collection_json["unique_properties"], list) or len(collection_json["unique_properties"]) == 0 or not all(
+            isinstance(elem, str) or 
+            (isinstance(elem, list) and all(isinstance(sub_elem, str) for sub_elem in elem))
+            for elem in collection_json["unique_properties"]
+        ):
+            raise PyDoughMetadataException(f"Property 'unique_properties' of {error_name} must be a non-empty list whose elements are all either strings or lists of strings.")
+        
 
 class GraphMetadata(object):
     """
     TODO: add class docstring
     """
-    def __init__(self, name : str, collections : List[CollectionMetadata]):
+    def __init__(self, name : str, collections : Dict[str, CollectionMetadata]):
         self.name : str = name
-        self.collections : Dict[str, CollectionMetadata] = {collection.name: collection for collection in collections}
+        self.collections : Dict[str, CollectionMetadata] = collections
         self.nouns : Dict[str, List[Union[CollectionMetadata, PropertyMetadata]]] = defaultdict(list)
+        self.nouns[self.name].append(self)
         for collection in self.collections.values():
-            collection.solidify_properties(self)
             for noun, value in collection.get_nouns():
                 self.nouns[noun].append(value) 
 
--- pydough/metadata/parse.py
+++ pydough/metadata/parse.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from pydough.metadata.graph import GraphMetadata, CollectionMetadata, PropertyMetadata
+from pydough.metadata.graph import GraphMetadata, CollectionMetadata, SimpleTableMetadata, PropertyMetadata, PyDoughMetadataException
 import json
 
 def parse_metadata(file_path : str, graph_name : str) -> GraphMetadata:
@@ -8,24 +8,80 @@ def parse_metadata(file_path : str, graph_name : str) -> GraphMetadata:
     """
     with open(file_path, "r") as f:
         as_json = json.load(f)
+    if not isinstance(as_json, dict):
+        raise PyDoughMetadataException(f"PyDough metadata expected to be a JSON file containing a JSON object.")
     if graph_name not in as_json:
-        raise Exception(f"PyDough metadata does not contain a graph named '{graph_name}'")
-    return parse_graph(graph_name, as_json[graph_name])
+        raise PyDoughMetadataException(f"PyDough metadata does not contain a graph named {repr(graph_name)}")
+    graph_json = as_json[graph_name]
+    verify_pydough_graph(graph_name, graph_json)
+    return parse_graph(graph_name, graph_json)
+
+def verify_pydough_graph(graph_name: str, graph_json : Dict) -> None:
+    """
+    TODO: add function doscstring.
+    """
+    if not isinstance(graph_json, dict):
+        raise PyDoughMetadataException(f"PyDough metadata for Graph {repr(graph_name)} must be a JSON object.")
+    for collection_name in graph_json:
+        verify_pydough_collection(graph_name, collection_name, graph_json)
+
+def verify_pydough_collection(graph_name: str, collection_name: str, graph_json : Dict) -> None:
+    """
+    TODO: add function doscstring.
+    """
+    if collection_name == graph_name:
+        raise PyDoughMetadataException(f"Cannot have collection named {repr(collection_name)} share the same name as the graph containing it.")
+    collection_json = graph_json[collection_name]
+    error_name = f"collection {repr(collection_name)} in graph {repr(graph_name)}"
+    if "type" not in collection_json:
+        raise PyDoughMetadataException(f"Metadata for {error_name} missing required property 'type'.")
+    if not isinstance(collection_json["type"], str):
+        raise PyDoughMetadataException(f"Property 'type' of {error_name} must be a string.")
+    match collection_json["type"]:
+        case "simple_table":
+            SimpleTableMetadata.verify_metadata(graph_name, collection_name, collection_json)
+        case collection_type:
+            raise PyDoughMetadataException(f"Unrecognized collection type for {error_name}: {repr(collection_type)}")
+        
+def verify_simple_table_collection(graph_name, collection_name, graph_json) -> None:
+    """
+    TODO: add function doscstring.
+    """
+    pass
 
 def parse_graph(graph_name : str, graph_json : Dict) -> GraphMetadata:
     """
     TODO: add function doscstring.
     """
-    collections = []
+
+    # A dictionary that will be used to map each collection name to the
+    # collection metadata object it corresponds to.
+    collections = {}
+
+    # A list that will store tuples corresponding to each collection property
+    # in the metadata before it is added to the collection, so all of the
+    # properties can be sorted based on their dependencies. The tuples
+    # are in the form (collection_name, property_name, property_json)
+    raw_properties = []
+
+    # Iterate through all the key-value pairs in the graph to set up the
+    # corresponding collections as empty metadata that will later be filled
+    # with properties.
     for collection_name in graph_json:
         if collection_name == graph_name:
             raise Exception(f"Cannot have collection named '{collection_name}' share the same name as the graph containing it.")
-        collections.append(parse_collection(collection_name, graph_json[collection_name]))
+        collection_json = graph_json[collection_name]
+        if "type" not in collection_json:
+            raise Exception(f"Collection metadata missing required property 'type'.")
+        if "properties" not in collection_json:
+            raise Exception(f"Collection metadata missing required property 'properties'.")
+        match collection_json["type"]:
+            case "simple_table":
+                collections[collection_name] = SimpleTableMetadata(collection_name)
+            case collection_type:
+                raise Exception(f"Unrecognized collection type: '{collection_type}'")
+        
+    for collection_name in graph_json:
+        collection = collection[collection_name]
+        collection.parse_from_json(graph_json[collection_name], collections)
     return GraphMetadata(graph_name, collections)
-
-def parse_collection(collection_name : str, collection_json : Dict) -> CollectionMetadata:
-    """
-    TODO: add function doscstring & finish the implementation.
-    """
-    collections = []
-    return CollectionMetadata(collection_name)
