6eca0dfc839d66977645bf1fcaa9f8c7e84ed6c6
knassre-bodo <kian@bodo.ai>
2024-10-18 14:21:25 -0400

    Adding get_property_names test

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,7 +3,9 @@ TODO: add file-level docstring
 """
 
 import pytest
-from pydough.metadata.graphs.graph_metadata import GraphMetadata
+from pydough.metadata.graphs import GraphMetadata
+from pydough.metadata.collections import CollectionMetadata
+from typing import List
 
 
 def test_graph_structure(sample_graphs):
@@ -48,9 +50,42 @@ def test_graph_structure(sample_graphs):
 )
 def test_get_collection_names(graph_name, answer, get_graph):
     """
-    Testing that the get_collection_names method of GraphMetadata correctly fetches
-    the names of all collections in the metadata for a graph.
+    Testing that the get_collection_names method of GraphMetadata correctly
+    fetches the names of all collections in the metadata for a graph.
     """
     graph: GraphMetadata = get_graph(graph_name)
     collection_names = graph.get_collection_names()
     assert sorted(collection_names) == sorted(answer)
+
+
+@pytest.mark.parametrize(
+    "graph_name, collection_name, answer",
+    [
+        pytest.param(
+            "amazon",
+            "Addresses",
+            [
+                "id",
+                "street_name",
+                "street_number",
+                "apartment",
+                "zip_code",
+                "city",
+                "state",
+                "occupancies",
+                "packages_shipped_to",
+                "packages_billed_to",
+            ],
+            id="amazon-addresses",
+        ),
+    ],
+)
+def test_get_property_names(graph_name, collection_name, answer, get_graph):
+    """
+    Testing that the get_property_names method of CollectionMetadata correctly
+    fetches the names of all properties in the metadata for a collection.
+    """
+    graph: GraphMetadata = get_graph(graph_name)
+    collection: CollectionMetadata = graph.get_collection(collection_name)
+    property_names: List[str] = collection.get_property_names()
+    assert sorted(property_names) == sorted(answer)
