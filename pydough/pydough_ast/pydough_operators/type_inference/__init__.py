0a2a3a8ea37c186759489fe20aab17baf43020bb
knassre-bodo <kian@bodo.ai>
2024-10-25 13:51:17 -0400

    Revisions [RUN CI]

--- pydough/pydough_ast/pydough_operators/type_inference/__init__.py
+++ pydough/pydough_ast/pydough_operators/type_inference/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["TypeVerifier", "AllowAny", "ExpressionTypeDeducer", "SelectArgumentType"]
 
 from .type_verifier import TypeVerifier, AllowAny
-from .type_deducer import ExpressionTypeDeducer, SelectArgumentType
+from .expression_type_deducer import ExpressionTypeDeducer, SelectArgumentType
