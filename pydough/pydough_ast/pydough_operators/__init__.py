1ab2d3904f428bc9ac2a2a1f261b586c2c6d43a8
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2024-10-29 13:15:20 -0400

    Set up type deducers in PyDough AST module (#14)
    Adds the type deduction base class to pydough.pydough_ast.pydough_operators.type_inferrence, with an implementation class that always selects the type of the nth-argument, and another that always returns a specific type.

--- pydough/pydough_ast/pydough_operators/__init__.py
+++ pydough/pydough_ast/pydough_operators/__init__.py
@@ -2,6 +2,20 @@
 TODO: add module-level docstring
 """
 
-__all__ = ["TypeVerifier", "AllowAny", "RequireNumArgs"]
+__all__ = [
+    "TypeVerifier",
+    "AllowAny",
+    "RequireNumArgs",
+    "ExpressionTypeDeducer",
+    "SelectArgumentType",
+    "ConstantType",
+]
 
-from .type_inference import TypeVerifier, AllowAny, RequireNumArgs
+from .type_inference import (
+    TypeVerifier,
+    AllowAny,
+    ExpressionTypeDeducer,
+    SelectArgumentType,
+    ConstantType,
+    RequireNumArgs,
+)
---
+++ tests/test_expression_type_deducer_errors.py
@@ -0,0 +1,40 @@
+"""
+TODO: add file-level docstring.
+"""
+
+from typing import List
+from pydough.pydough_ast import PyDoughAST, PyDoughASTException
+from pydough.pydough_ast.pydough_operators import (
+    ExpressionTypeDeducer,
+    SelectArgumentType,
+)
+import re
+import pytest
+
+
+@pytest.mark.parametrize(
+    "deducer, args, error_message",
+    [
+        pytest.param(
+            SelectArgumentType(0),
+            [],
+            re.escape("Cannot select type of argument 0 out of []"),
+            id="select_0-empty_args",
+        ),
+        pytest.param(
+            SelectArgumentType(-1),
+            [],
+            re.escape("Cannot select type of argument -1 out of []"),
+            id="select_invalid-empty_args",
+        ),
+    ],
+)
+def test_invalid_deduction(
+    deducer: ExpressionTypeDeducer, args: List[PyDoughAST], error_message: str
+):
+    """
+    Checks cases where calling an expression type deducer on a list of PyDough
+    AST objects should raise an exception
+    """
+    with pytest.raises(PyDoughASTException, match=error_message):
+        deducer.infer_return_type(args)
