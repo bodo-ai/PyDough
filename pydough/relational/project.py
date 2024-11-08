cb32b8979ecdbfdff80864117dd6370a1c78285a
Nicholas <njriasanovsky@berkeley.edu>
2024-11-08 09:52:41 -0500

    Added the class defintion

--- pydough/relational/project.py
+++ pydough/relational/project.py
@@ -5,3 +5,13 @@ adding or removing columns (as well as technically reordering). In general, we s
 avoid introducing extra nodes just to reorder or prune columns, so ideally their use
 should be sparse.
 """
+
+from .abstract import Relational
+
+
+class Project(Relational):
+    """
+    The Project node in the relational tree. This node represents a "calc" in
+    relational algebra, which should involve some "compute" functions and may
+    involve adding, removing, or reordering columns.
+    """
