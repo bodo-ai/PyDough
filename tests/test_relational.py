24b620e7b7180c07d1e8b40e0c73613e8b142dac
Nicholas <njriasanovsky@berkeley.edu>
2024-11-08 16:01:24 -0500

    Added error test

--- tests/test_relational.py
+++ tests/test_relational.py
@@ -88,7 +88,7 @@ def test_scan_inputs():
         ),
     ],
 )
-def test_scan_to_string(scan_node, output):
+def test_scan_to_string(scan_node: Scan, output: str):
     assert scan_node.to_string() == output
 
 
@@ -143,41 +143,87 @@ def test_scan_to_string(scan_node, output):
         ),
     ],
 )
-def test_scan_equals(first_scan, second_scan, output):
+def test_scan_equals(first_scan: Scan, second_scan: Scan, output: bool):
     assert first_scan.equals(second_scan) == output
 
 
-# @pytest.mark.parametrize(
-#     "first_scan, second_scan, output",
-#     [
-#         pytest.param(
-#             id="different_table",
-#         ),
-#         pytest.param(
-#             id="matching_columns",
-#         ),
-#         pytest.param(
-#             id="disjoint_columns",
-#         ),
-#         pytest.param(
-#             id="overlapping_columns",
-#         ),
-#         pytest.param(
-#             id="matching_orderings",
-#         ),
-#         pytest.param(
-#             id="disjoint_orderings",
-#         ),
-#         pytest.param(
-#             id="overlapping_orderings",
-#         ),
-#         pytest.param(
-#             id="conflicting_orderings",
-#         ),
-#     ],
-# )
-# def test_scan_can_merge():
-#     pass
+@pytest.mark.parametrize(
+    "first_scan, second_scan, output",
+    [
+        pytest.param(
+            Scan("table1", [make_column("a"), make_column("b")]),
+            Scan("table2", [make_column("a"), make_column("b")]),
+            False,
+            id="different_table",
+        ),
+        pytest.param(
+            Scan("table1", [make_column("a"), make_column("b")]),
+            Scan("table1", [make_column("a"), make_column("b")]),
+            True,
+            id="matching_columns",
+        ),
+        pytest.param(
+            Scan("table1", [make_column("a"), make_column("b")]),
+            Scan("table1", [make_column("c"), make_column("d")]),
+            True,
+            id="disjoint_columns",
+        ),
+        pytest.param(
+            Scan("table1", [make_column("a"), make_column("b")]),
+            Scan("table1", [make_column("b"), make_column("c")]),
+            True,
+            id="overlapping_columns",
+        ),
+        pytest.param(
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            # Note: Eventually this should be legal
+            False,
+            id="matching_orderings",
+        ),
+        pytest.param(
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("b")],
+            ),
+            False,
+            id="disjoint_orderings",
+        ),
+        pytest.param(
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a"), make_simple_column_reference("b")],
+            ),
+            # Note: If we allow merging orderings this should become legal.
+            False,
+            id="overlapping_orderings",
+        ),
+        # TODO: Add a conflicting ordering test where A is ASC vs DESC.
+        # Depends on other code changes merging.
+    ],
+)
+def test_scan_can_merge(first_scan: Scan, second_scan: Scan, output: bool):
+    assert first_scan.can_merge(second_scan) == output
 
 
 # @pytest.mark.parametrize(
@@ -201,22 +247,59 @@ def test_scan_equals(first_scan, second_scan, output):
 #     pass
 
 
-# @pytest.mark.parametrize(
-#     "first_scan, second_scan, output",
-#     [
-#         pytest.param(
-#             id="different_table",
-#         ),
-#         pytest.param(
-#             id="disjoint_orderings",
-#         ),
-#         pytest.param(
-#             id="overlapping_orderings",
-#         ),
-#         pytest.param(
-#             id="conflicting_orderings",
-#         ),
-#     ],
-# )
-# def test_scan_invalid_merge():
-#     pass
+@pytest.mark.parametrize(
+    "first_scan, second_scan",
+    [
+        pytest.param(
+            Scan("table1", [make_column("a"), make_column("b")]),
+            Scan("table2", [make_column("a"), make_column("b")]),
+            id="different_table",
+        ),
+        pytest.param(
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            # Note: Eventually this should be legal
+            id="matching_orderings",
+        ),
+        pytest.param(
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("b")],
+            ),
+            id="disjoint_orderings",
+        ),
+        pytest.param(
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a")],
+            ),
+            Scan(
+                "table1",
+                [make_column("a"), make_column("b")],
+                [make_simple_column_reference("a"), make_simple_column_reference("b")],
+            ),
+            # Note: If we allow merging orderings this should become legal.
+            id="overlapping_orderings",
+        ),
+        # TODO: Add a conflicting ordering test where A is ASC vs DESC.
+        # Depends on other code changes merging.
+    ],
+)
+def test_scan_invalid_merge(first_scan: Scan, second_scan: Scan):
+    with pytest.raises(ValueError, match="Cannot merge nodes"):
+        first_scan.merge(second_scan)
