0a6ff798133a171a778fd61e9a1b2a89990b89c0
Nick Riasanovsky <nick@bodo.ai>
2024-11-20 21:39:55 -0500

    [BSE-4250] Create relational to SQL tests for all node types but join (#68)

--- tests/test_relational_to_sql.py
+++ tests/test_relational_to_sql.py
@@ -12,14 +12,17 @@ from test_utils import (
     make_relational_literal,
 )
 
-from pydough.pydough_ast.pydough_operators import ADD
-from pydough.relational.relational_expressions import CallExpression
+from pydough.pydough_ast.pydough_operators import ADD, EQU, SUM
+from pydough.relational.relational_expressions import CallExpression, LiteralExpression
 from pydough.relational.relational_nodes import (
+    Aggregate,
+    Filter,
+    Limit,
     Project,
     RelationalRoot,
 )
 from pydough.sqlglot import convert_relation_to_sql
-from pydough.types import Int64Type
+from pydough.types import BooleanType, Int64Type
 
 
 @pytest.fixture(scope="module")
@@ -86,6 +89,134 @@ def sqlite_dialect() -> SQLiteDialect:
             ),
             "SELECT b FROM (SELECT a, b, a + 1 AS c FROM (SELECT a, b FROM table)) ORDER BY c",
             id="project_scan_with_ordering",
+        ),
+        pytest.param(
+            RelationalRoot(
+                ordered_columns=[
+                    ("a", make_relational_column_reference("a")),
+                    ("b", make_relational_column_reference("b")),
+                ],
+                input=Filter(
+                    input=build_simple_scan(),
+                    columns={
+                        "a": make_relational_column_reference("a"),
+                        "b": make_relational_column_reference("b"),
+                    },
+                    condition=CallExpression(
+                        EQU,
+                        BooleanType(),
+                        [
+                            make_relational_column_reference("a"),
+                            make_relational_literal(1, Int64Type()),
+                        ],
+                    ),
+                ),
+            ),
+            "SELECT a, b FROM table WHERE a = 1",
+            id="simple_filter",
+        ),
+        pytest.param(
+            RelationalRoot(
+                ordered_columns=[
+                    ("a", make_relational_column_reference("a")),
+                    ("b", make_relational_column_reference("b")),
+                ],
+                input=Limit(
+                    input=build_simple_scan(),
+                    limit=LiteralExpression(1, Int64Type()),
+                    columns={
+                        "a": make_relational_column_reference("a"),
+                        "b": make_relational_column_reference("b"),
+                    },
+                ),
+            ),
+            "SELECT a, b FROM table LIMIT 1",
+            id="simple_limit",
+        ),
+        pytest.param(
+            RelationalRoot(
+                ordered_columns=[
+                    ("a", make_relational_column_reference("a")),
+                    ("b", make_relational_column_reference("b")),
+                ],
+                input=Limit(
+                    input=build_simple_scan(),
+                    limit=LiteralExpression(10, Int64Type()),
+                    columns={
+                        "a": make_relational_column_reference("a"),
+                        "b": make_relational_column_reference("b"),
+                    },
+                    orderings=[
+                        make_relational_column_ordering(
+                            make_relational_column_reference("a"),
+                            ascending=True,
+                            nulls_first=True,
+                        ),
+                        make_relational_column_ordering(
+                            make_relational_column_reference("b"),
+                            ascending=False,
+                            nulls_first=False,
+                        ),
+                    ],
+                ),
+            ),
+            "SELECT a, b FROM table ORDER BY a, b DESC LIMIT 10",
+            id="simple_limit_with_ordering",
+        ),
+        pytest.param(
+            RelationalRoot(
+                ordered_columns=[
+                    ("b", make_relational_column_reference("b")),
+                ],
+                input=Aggregate(
+                    input=build_simple_scan(),
+                    keys={
+                        "b": make_relational_column_reference("b"),
+                    },
+                    aggregations={},
+                ),
+            ),
+            "SELECT b FROM table GROUP BY b",
+            id="simple_distinct",
+        ),
+        pytest.param(
+            RelationalRoot(
+                ordered_columns=[
+                    ("a", make_relational_column_reference("a")),
+                ],
+                input=Aggregate(
+                    input=build_simple_scan(),
+                    keys={},
+                    aggregations={
+                        "a": CallExpression(
+                            SUM, Int64Type(), [make_relational_column_reference("a")]
+                        )
+                    },
+                ),
+            ),
+            "SELECT SUM(a) AS a FROM (SELECT a, b FROM table)",
+            id="simple_sum",
+        ),
+        pytest.param(
+            RelationalRoot(
+                ordered_columns=[
+                    ("a", make_relational_column_reference("a")),
+                    ("b", make_relational_column_reference("b")),
+                ],
+                input=Aggregate(
+                    input=build_simple_scan(),
+                    keys={
+                        "b": make_relational_column_reference("b"),
+                    },
+                    aggregations={
+                        "a": CallExpression(
+                            SUM, Int64Type(), [make_relational_column_reference("a")]
+                        )
+                    },
+                ),
+            ),
+            "SELECT SUM(a) AS a, b FROM (SELECT a, b FROM table) GROUP BY b",
+            id="simple_groupby_sum",
         ),
     ],
 )
