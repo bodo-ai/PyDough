f0969c23bc52a68ab27a2026c4c8b10f37271998
Nicholas <njriasanovsky@berkeley.edu>
2024-11-22 00:56:23 -0500

    Fixed q1

--- tests/test_relational_execution_tpch.py
+++ tests/test_relational_execution_tpch.py
@@ -5,8 +5,12 @@ Simple tests to run TPC-H queries on SQLite.
 from typing import Any
 
 import pytest
-from tpch_outputs import tpch_q6_output
-from tpch_relational_plans import tpch_query_6_plan
+from tpch_outputs import tpch_q1_output, tpch_q3_output, tpch_q6_output
+from tpch_relational_plans import (
+    tpch_query_1_plan,
+    tpch_query_3_plan,
+    tpch_query_6_plan,
+)
 
 from pydough.database_connectors import DatabaseContext
 from pydough.relational import RelationalRoot
@@ -16,6 +20,16 @@ from pydough.sqlglot import execute
 @pytest.mark.parametrize(
     "root, output",
     [
+        pytest.param(
+            tpch_query_1_plan(),
+            tpch_q1_output(),
+            id="tpch_q1",
+        ),
+        pytest.param(
+            tpch_query_3_plan(),
+            tpch_q3_output(),
+            id="tpch_q3",
+        ),
         pytest.param(
             tpch_query_6_plan(),
             tpch_q6_output(),
