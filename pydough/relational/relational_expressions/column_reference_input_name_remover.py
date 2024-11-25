00d7b7f18fc942832fbe91c73d1e46492254f572
Nicholas <njriasanovsky@berkeley.edu>
2024-11-25 10:42:17 -0500

    Moved call expression into parent [run CI]

--- pydough/relational/relational_expressions/column_reference_input_name_remover.py
+++ pydough/relational/relational_expressions/column_reference_input_name_remover.py
@@ -4,7 +4,6 @@ any column reference that doesn't fall within a given set.
 """
 
 from .abstract_expression import RelationalExpression
-from .call_expression import CallExpression
 from .column_reference import ColumnReference
 from .literal_expression import LiteralExpression
 from .relational_expression_shuttle import RelationalExpressionShuttle
@@ -23,12 +22,6 @@ class ColumnReferenceInputNameRemover(RelationalExpressionShuttle):
 
     def set_kept_names(self, kept_names: set[str]) -> None:
         self._kept_names = kept_names
-
-    def visit_call_expression(
-        self, call_expression: CallExpression
-    ) -> RelationalExpression:
-        args = [args.accept_shuttle(self) for args in call_expression.inputs]
-        return CallExpression(call_expression.op, call_expression.data_type, args)
 
     def visit_literal_expression(
         self, literal_expression: LiteralExpression
