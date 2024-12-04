dc37c1d865212320f70817aa8da063bcbe1fcf67
Nicholas <njriasanovsky@berkeley.edu>
2024-12-04 13:33:47 -0500

    Added the metadata test

--- tests/test_session.py
+++ tests/test_session.py
@@ -9,6 +9,7 @@ manipulate the session without affecting other tests.
 
 from pydough.configs import PyDoughConfigs, PyDoughSession
 from pydough.database_connectors import DatabaseDialect, empty_connection
+from pydough.metadata import GraphMetadata
 
 
 def test_defaults() -> None:
@@ -26,3 +27,18 @@ def test_defaults() -> None:
     assert session.database is not None
     assert session.database.connection is empty_connection
     assert session.database.dialect is DatabaseDialect.ANSI
+
+
+def test_load_metadata_graph(sample_graph_path: str, sample_graph_names: str) -> None:
+    """
+    Tests that we can load the metadata graph on the session.
+    Also checks a couple features to ensure we have loaded the correct graph.
+    """
+    session: PyDoughSession = PyDoughSession()
+    graph: GraphMetadata = session.load_metadata_graph(
+        sample_graph_path, sample_graph_names
+    )
+    assert graph is session.metadata
+    assert graph.name == sample_graph_names
+    assert graph.path == sample_graph_names
+    assert graph.components == [sample_graph_names]
