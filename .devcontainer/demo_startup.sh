d8ae3188f7de1ac7387cc7a6faffb547a3720972
Nicholas <njriasanovsky@berkeley.edu>
2024-12-16 11:19:33 -0500

    Moved to a setup script

---
+++ .devcontainer/demo_startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eo pipefail
+
+# Download the TPC-H database
+./demos/setup_tpch.sh ./demos/tpch.db
+
+# Build the PyDough project
+python -m pip install .
+
+# Build the Juypter Extension
+cd pydough_jupyter_extensions && python -m pip install .
+
+# Open the readme file
+code -r demos/README.md --open-to-side
