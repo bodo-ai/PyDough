4de2bacbd628133f05d3e65c5422a2aace58c291
Nick Riasanovsky <nick@bodo.ai>
2024-12-17 10:19:13 -0500

    [BSE-4381] Move the download to a cached step (#133)

--- .devcontainer/demo_startup.sh
+++ .devcontainer/demo_startup.sh
@@ -2,8 +2,9 @@
 
 set -eo pipefail
 
-# Download the TPC-H database
-./demos/setup_tpch.sh ./demos/tpch.db
+# Link the TPC-H database. Use a symlink to
+# avoid copying the data at launch.
+ln -s /usr/local/share/tpch.db demos/tpch.db
 
 # Build the PyDough project
 python -m pip install .
