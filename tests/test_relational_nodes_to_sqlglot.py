77690262956e5dd39e27b3ec34cf860a5e6a8dba
Vineet <vineet.gandham@gmail.com>
2025-02-04 07:47:16 -0800

    [RUN CI]

--- tests/test_relational_nodes_to_sqlglot.py
+++ tests/test_relational_nodes_to_sqlglot.py
@@ -615,8 +615,8 @@ def mkglot_func(op: type[Expression], args: list[Expression]) -> Expression:
             ),
             mkglot(
                 expressions=[
-                    Ident(this="b"),
                     set_glot_alias(mk_literal(1, False), "c"),
+                    Ident(this="b"),
                 ],
                 _from=GlotFrom(Table(this=Ident(this="table"))),
                 limit=mk_literal(2, False),
@@ -678,11 +678,11 @@ def mkglot_func(op: type[Expression], args: list[Expression]) -> Expression:
             ),
             mkglot(
                 expressions=[
-                    Ident(this="b"),
                     set_glot_alias(
                         mkglot_func(Sum, [Ident(this="a")]),
                         "a",
                     ),
+                    Ident(this="b"),
                 ],
                 group_by=[Ident(this="b")],
                 _from=GlotFrom(
@@ -759,11 +759,11 @@ def mkglot_func(op: type[Expression], args: list[Expression]) -> Expression:
                 _from=GlotFrom(
                     mkglot(
                         expressions=[
-                            Ident(this="b"),
                             set_glot_alias(
                                 mkglot_func(Sum, [Ident(this="a")]),
                                 "a",
                             ),
+                            Ident(this="b"),
                         ],
                         group_by=[Ident(this="b")],
                         _from=GlotFrom(
@@ -1297,14 +1297,14 @@ def mkglot_func(op: type[Expression], args: list[Expression]) -> Expression:
                 _from=GlotFrom(
                     mkglot(
                         expressions=[
-                            Ident(this="a"),
-                            Ident(this="b"),
                             set_glot_alias(
                                 mkglot_func(
                                     Add, [Ident(this="a"), mk_literal(1, False)]
                                 ),
                                 "c",
                             ),
+                            Ident(this="a"),
+                            Ident(this="b"),
                         ],
                         _from=GlotFrom(
                             mkglot(
@@ -1418,11 +1418,11 @@ def mkglot_func(op: type[Expression], args: list[Expression]) -> Expression:
                 _from=GlotFrom(
                     mkglot(
                         expressions=[
-                            Ident(this="b"),
                             set_glot_alias(
                                 mkglot_func(Sum, [Ident(this="a")]),
                                 "a",
                             ),
+                            Ident(this="b"),
                         ],
                         group_by=[Ident(this="b")],
                         _from=GlotFrom(
@@ -1588,7 +1588,6 @@ def test_node_to_sqlglot(
     sqlglot_relational_visitor.reset()
     node.accept(sqlglot_relational_visitor)
     actual = sqlglot_relational_visitor.get_sqlglot_result()
-    breakpoint()
     assert actual == sqlglot_expr
 
 
