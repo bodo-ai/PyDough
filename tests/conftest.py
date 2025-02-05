9de62e77712fe9331986a2aab2298b19133b496f
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2025-02-05 15:35:13 -0500

    Update planner tests to store answers in a refsol file (#243)
    See #242 for details

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ available.
 import json
 import os
 import sqlite3
-from collections.abc import MutableMapping
+from collections.abc import Callable, MutableMapping
 
 import pytest
 from test_utils import graph_fetcher, map_over_dict_values, noun_fetcher
@@ -137,6 +137,31 @@ def tpch_node_builder(get_sample_graph) -> AstNodeBuilder:
     Builds a QDAG node builder using the TPCH graoh.
     """
     return AstNodeBuilder(get_sample_graph("TPCH"))
+
+
+@pytest.fixture(scope="session")
+def get_plan_test_filename() -> Callable[[str], str]:
+    """
+    A function that takes in a file name and returns the path to that file
+    from within the directory of plan testing refsol files.
+    """
+
+    def impl(file_name: str) -> str:
+        return f"{os.path.dirname(__file__)}/test_plan_refsols/{file_name}.txt"
+
+    return impl
+
+
+@pytest.fixture
+def update_plan_tests() -> bool:
+    """
+    If True, planner tests should update the refsol file instead of verifying
+    that the test matches the file. If False, the refsol file is used to check
+    the answer.
+
+    This is controlled by an environment variable `PYDOUGH_UPDATE_TESTS`.
+    """
+    return os.getenv("PYDOUGH_UPDATE_TESTS", "0") == "1"
 
 
 @pytest.fixture(
