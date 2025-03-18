d8f9509738e24a990f69f9547a4a79aea6a41e26
knassre-bodo <kian@bodo.ai>
2025-03-18 12:38:41 -0400

    Added SQL refsols for tpch queries

---
+++ tests/test_sql_refsols/tpch_q4_ansi.sql
@@ -0,0 +1,55 @@
+SELECT
+  O_ORDERPRIORITY,
+  ORDER_COUNT
+FROM (
+  SELECT
+    COALESCE(agg_0, 0) AS ORDER_COUNT,
+    order_priority AS O_ORDERPRIORITY,
+    order_priority AS ordering_1
+  FROM (
+    SELECT
+      COUNT() AS agg_0,
+      order_priority
+    FROM (
+      SELECT
+        order_priority
+      FROM (
+        SELECT
+          key,
+          order_priority
+        FROM (
+          SELECT
+            o_orderdate AS order_date,
+            o_orderkey AS key,
+            o_orderpriority AS order_priority
+          FROM tpch.ORDERS
+        )
+        WHERE
+          (
+            order_date < DATE_STR_TO_DATE('1993-10-01')
+          )
+          AND (
+            order_date >= DATE_STR_TO_DATE('1993-07-01')
+          )
+      )
+      SEMI JOIN (
+        SELECT
+          order_key
+        FROM (
+          SELECT
+            l_commitdate AS commit_date,
+            l_orderkey AS order_key,
+            l_receiptdate AS receipt_date
+          FROM tpch.LINEITEM
+        )
+        WHERE
+          commit_date < receipt_date
+      )
+        ON key = order_key
+    )
+    GROUP BY
+      order_priority
+  )
+)
+ORDER BY
+  ordering_1
