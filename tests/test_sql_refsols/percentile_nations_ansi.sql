b503f8a3686c71ff8545eadb5d2163a029850244
knassre-bodo <kian@bodo.ai>
2025-05-30 15:51:10 -0700

    Reverting creation of more SQL files

--- tests/test_sql_refsols/percentile_nations_ansi.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  n_name AS name,
-  NTILE(5) OVER (ORDER BY n_name NULLS LAST) AS p1,
-  NTILE(5) OVER (ORDER BY n_name NULLS LAST) AS p2
-FROM tpch.nation
--- tests/test_sql_refsols/simple_filter_top_five_ansi.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  o_orderkey AS key
-FROM tpch.orders
-WHERE
-  o_totalprice < 1000.0
-ORDER BY
-  key DESC
-LIMIT 5
