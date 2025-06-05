b460eb4b01a4f1fef776dc5ad97e0f339cf0ae36
knassre-bodo <kian@bodo.ai>
2025-06-05 17:25:17 -0400

    Updating bad offender tests

--- tests/test_sql_refsols/technograph_incident_rate_by_release_year_sqlite.sql
+++ tests/test_sql_refsols/technograph_incident_rate_by_release_year_sqlite.sql
@@ -1,4 +1,4 @@
-WITH _t2 AS (
+WITH _t3 AS (
   SELECT
     pr_id AS _id,
     pr_release AS release_date
@@ -6,29 +6,29 @@ WITH _t2 AS (
 ), _s6 AS (
   SELECT
     COUNT() AS agg_1,
-    CAST(STRFTIME('%Y', _t2.release_date) AS INTEGER) AS year
+    CAST(STRFTIME('%Y', _t3.release_date) AS INTEGER) AS release_year
   FROM main.devices AS devices
-  JOIN _t2 AS _t2
-    ON _t2._id = devices.de_product_id
+  JOIN _t3 AS _t3
+    ON _t3._id = devices.de_product_id
   GROUP BY
-    CAST(STRFTIME('%Y', _t2.release_date) AS INTEGER)
+    CAST(STRFTIME('%Y', _t3.release_date) AS INTEGER)
 ), _s7 AS (
   SELECT
     COUNT() AS agg_0,
-    CAST(STRFTIME('%Y', _t4.release_date) AS INTEGER) AS year
+    CAST(STRFTIME('%Y', _t5.release_date) AS INTEGER) AS release_year
   FROM main.devices AS devices
-  JOIN _t2 AS _t4
-    ON _t4._id = devices.de_product_id
+  JOIN _t3 AS _t5
+    ON _t5._id = devices.de_product_id
   JOIN main.incidents AS incidents
     ON devices.de_id = incidents.in_device_id
   GROUP BY
-    CAST(STRFTIME('%Y', _t4.release_date) AS INTEGER)
+    CAST(STRFTIME('%Y', _t5.release_date) AS INTEGER)
 )
 SELECT
-  _s6.year,
+  _s6.release_year AS year,
   ROUND(CAST(COALESCE(_s7.agg_0, 0) AS REAL) / _s6.agg_1, 2) AS ir
 FROM _s6 AS _s6
 LEFT JOIN _s7 AS _s7
-  ON _s6.year = _s7.year
+  ON _s6.release_year = _s7.release_year
 ORDER BY
   year
