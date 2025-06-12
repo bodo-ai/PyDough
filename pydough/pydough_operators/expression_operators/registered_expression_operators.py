9ee3e6a5217d0f43a302ae43b119bce5e567838c
john-sanchez31 <johnsanchez@bodo.ai>
2025-06-12 13:05:45 -0600

    formatting [RUN CI]

--- pydough/pydough_operators/expression_operators/registered_expression_operators.py
+++ pydough/pydough_operators/expression_operators/registered_expression_operators.py
@@ -142,7 +142,7 @@ REPLACE = ExpressionFunctionOperator(
     "REPLACE", False, RequireArgRange(2, 3), SelectArgumentType(0)
 )
 STRCOUNT = ExpressionFunctionOperator(
-    "STRCOUNT", False, RequireArgRange(2), ConstantType(NumericType())
+    "STRCOUNT", False, RequireNumArgs(2), ConstantType(NumericType())
 )
 ENDSWITH = ExpressionFunctionOperator(
     "ENDSWITH", False, RequireNumArgs(2), ConstantType(BooleanType())
