5aecdaf99a20ebf10ca0a6e5f15c579ea4d1012b
Hadia Ahmed <hadia@bodo.ai>
2025-06-20 13:18:35 -0700

    use 1:1 mappings

--- pydough/sqlglot/transform_bindings/sf_transform_bindings.py
+++ pydough/sqlglot/transform_bindings/sf_transform_bindings.py
@@ -29,40 +29,28 @@ class SnowflakeTransformBindings(BaseTransformBindings):
         match operator:
             case pydop.SUM:
                 return self.convert_sum(args, types)
-            case pydop.STARTSWITH:
-                return sqlglot_expressions.Anonymous(
-                    this="STARTSWITH", expressions=args
-                )
-            case pydop.ENDSWITH:
-                return sqlglot_expressions.Anonymous(this="ENDSWITH", expressions=args)
-            case pydop.CONTAINS:
-                return sqlglot_expressions.Anonymous(this="CONTAINS", expressions=args)
-            case pydop.LPAD:
-                return sqlglot_expressions.Anonymous(this="LPAD", expressions=args)
-            case pydop.RPAD:
-                return sqlglot_expressions.Anonymous(this="RPAD", expressions=args)
-            case pydop.SIGN:
-                return sqlglot_expressions.Anonymous(this="SIGN", expressions=args)
-            case pydop.YEAR:
-                return sqlglot_expressions.Anonymous(this="YEAR", expressions=args)
-            case pydop.QUARTER:
-                return sqlglot_expressions.Anonymous(this="QUARTER", expressions=args)
-            case pydop.MONTH:
-                return sqlglot_expressions.Anonymous(this="MONTH", expressions=args)
-            case pydop.DAY:
-                return sqlglot_expressions.Anonymous(this="DAY", expressions=args)
-            case pydop.HOUR:
-                return sqlglot_expressions.Anonymous(this="HOUR", expressions=args)
-            case pydop.MINUTE:
-                return sqlglot_expressions.Anonymous(this="MINUTE", expressions=args)
-            case pydop.SECOND:
-                return sqlglot_expressions.Anonymous(this="SECOND", expressions=args)
-            case pydop.DAYNAME:
-                return sqlglot_expressions.Anonymous(this="DAYNAME", expressions=args)
-            case pydop.SMALLEST:
-                return sqlglot_expressions.Anonymous(this="LEAST", expressions=args)
-            case pydop.LARGEST:
-                return sqlglot_expressions.Anonymous(this="GREATEST", expressions=args)
+        PYDOP_TO_SNOWFLAKE_FUNC: dict[pydop.PyDoughExpressionOperator, str] = {
+            pydop.STARTSWITH: "STARTSWITH",
+            pydop.ENDSWITH: "ENDSWITH",
+            pydop.CONTAINS: "CONTAINS",
+            pydop.LPAD: "LPAD",
+            pydop.RPAD: "RPAD",
+            pydop.SIGN: "SIGN",
+            pydop.YEAR: "YEAR",
+            pydop.QUARTER: "QUARTER",
+            pydop.MONTH: "MONTH",
+            pydop.DAY: "DAY",
+            pydop.HOUR: "HOUR",
+            pydop.MINUTE: "MINUTE",
+            pydop.SECOND: "SECOND",
+            pydop.DAYNAME: "DAYNAME",
+            pydop.SMALLEST: "LEAST",
+            pydop.LARGEST: "GREATEST",
+        }
+        if operator in PYDOP_TO_SNOWFLAKE_FUNC:
+            return sqlglot_expressions.Anonymous(
+                this=PYDOP_TO_SNOWFLAKE_FUNC[operator], expressions=args
+            )
 
         return super().convert_call_to_sqlglot(operator, args, types)
 
