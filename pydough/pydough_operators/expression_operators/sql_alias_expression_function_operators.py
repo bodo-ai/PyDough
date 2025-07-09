48e875faa93f918175e0154594aa82449bb45d55
knassre-bodo <kian@bodo.ai>
2025-07-09 17:33:50 -0400

    [RUN CI]

--- pydough/pydough_operators/expression_operators/sql_alias_expression_function_operators.py
+++ pydough/pydough_operators/expression_operators/sql_alias_expression_function_operators.py
@@ -8,8 +8,6 @@ __all__ = ["SqlAliasExpressionFunctionOperator"]
 
 
 from pydough.pydough_operators.type_inference import (
-    ExpressionTypeDeducer,
-    TypeVerifier,
     build_deducer_from_json,
     build_verifier_from_json,
 )
@@ -36,23 +34,10 @@ class SqlAliasExpressionFunctionOperator(ExpressionFunctionOperator):
         deducer_json: dict | None,
         description: str | None,
     ):
-        verifier: TypeVerifier = build_verifier_from_json(verifier_json)
-        deducer: ExpressionTypeDeducer = build_deducer_from_json(deducer_json)
+        build_verifier_from_json(verifier_json)
+        build_deducer_from_json(deducer_json)
         self._sql_function_alias: str = sql_function_alias
         self._description: str | None = description
-        super().__init__(function_name, is_aggregation, verifier, deducer, True)
-
-    @property
-    def key(self) -> str:
-        return f"FUNCTION-{self.function_name}"
-
-    @property
-    def is_aggregation(self) -> bool:
-        return self._is_aggregation
-
-    @property
-    def function_name(self) -> str:
-        return self._function_name
 
     @property
     def sql_function_alias(self) -> str:
@@ -61,17 +46,6 @@ class SqlAliasExpressionFunctionOperator(ExpressionFunctionOperator):
     @property
     def description(self) -> str | None:
         return self._description
-
-    @property
-    def standalone_string(self) -> str:
-        return f"Function[{self.function_name}]"
-
-    def requires_enclosing_parens(self, parent) -> bool:
-        return False
-
-    def to_string(self, arg_strings: list[str]) -> str:
-        # Stringify as "function_name(arg0, arg1, ...)
-        return f"{self.function_name}({', '.join(arg_strings)})"
 
     def equals(self, other: object) -> bool:
         return (
