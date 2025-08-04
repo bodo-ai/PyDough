6ed0e72a850a62cd10f6a59c11b21564b9b6e521
knassre-bodo <kian@bodo.ai>
2025-08-04 11:35:48 -0400

    Add masked table column metadata

---
+++ pydough/metadata/properties/masked_table_column_metadata.py
@@ -0,0 +1,194 @@
+"""
+Definition of the class for PyDough metadata for properties that access a
+column of a table from a relational system that has been masked by a certain
+protocol, but has an un-masking protocol as well.
+"""
+
+__all__ = ["MaskedTableColumnMetadata"]
+
+
+from pydough.metadata.collections import CollectionMetadata
+from pydough.metadata.errors import (
+    NoExtraKeys,
+    PyDoughMetadataException,
+    extract_array,
+    extract_bool,
+    extract_object,
+    extract_string,
+)
+from pydough.types import PyDoughType, parse_type_from_string
+from pydough.types.errors import PyDoughTypeException
+
+from .table_column_metadata import TableColumnMetadata
+
+
+class MaskedTableColumnMetadata(TableColumnMetadata):
+    """
+    Concrete metadata implementation for a PyDough property representing a
+    column of data from a relational table that has been masked by a certain
+    protocol, but has an un-masking protocol as well.
+    """
+
+    # Set of names of fields that can be included in the JSON object
+    # describing a table column property.
+    allowed_fields: set[str] = TableColumnMetadata.allowed_fields | {
+        "protect protocol",
+        "unprotect protocol",
+        "server masked",
+    }
+
+    def __init__(
+        self,
+        name: str,
+        collection: CollectionMetadata,
+        data_type: PyDoughType,
+        column_name: str,
+        unprotect_protocol: str,
+        protect_protocol: str,
+        server_masked: bool,
+        sample_values: list | None = None,
+        description: str | None = None,
+        synonyms: list[str] | None = None,
+        extra_semantic_info: dict | None = None,
+    ):
+        super().__init__(
+            name,
+            collection,
+            data_type,
+            column_name,
+            sample_values,
+            description,
+            synonyms,
+            extra_semantic_info,
+        )
+        self._unprotect_protocol: str = unprotect_protocol
+        self._protect_protocol: str = protect_protocol
+        self._server_masked: bool = server_masked
+
+    @property
+    def unprotect_protocol(self) -> str:
+        """
+        Returns the format string used to un-mask the data in this
+        table column property. The format string should create SQL text such
+        that when `self.unprotect_protocol.format(value)` is called, where
+        `value` is the SQL text string corresponding to a value that is masked,
+        it returns the SQL text string that will un-mask the value.
+        """
+        return self._unprotect_protocol
+
+    @property
+    def protect_protocol(self) -> str:
+        """
+        Returns the format string used to mask the data in this table column
+        property. The format string should create SQL text such that when
+        `self.protect_protocol.format(value)` is called, where `value` is the
+        SQL text string corresponding to a value that is to be masked, it
+        returns the SQL text string that will mask the value in a way that can
+        be reversed by `self.unprotect_protocol`.
+        """
+        return self._protect_protocol
+
+    @property
+    def server_masked(self) -> bool:
+        """
+        Returns whether the data in this table column property is masked in
+        a manner that corresponds to a server that can be sent predicate queries
+        in order to infer smarter predicates that do not require un-masking the
+        data.
+        """
+        return self._server_masked
+
+    @property
+    def column_name(self) -> str:
+        return self._column_name
+
+    @staticmethod
+    def create_error_name(name: str, collection_error_name: str):
+        return f"masked table column property {name!r} of {collection_error_name}"
+
+    @property
+    def components(self) -> list:
+        comp: list = super().components
+        comp.append(self.column_name)
+        comp.append(self.unprotect_protocol)
+        comp.append(self.protect_protocol)
+        comp.append(self.server_masked)
+        return comp
+
+    @staticmethod
+    def parse_from_json(
+        collection: CollectionMetadata, property_name: str, property_json: dict
+    ) -> None:
+        """
+        Procedure dispatched from PropertyMetadata.parse_from_json to handle
+        the parsing for table column properties.
+
+        Args:
+            `collection`: the metadata for the PyDough collection that the
+            property would be inserted into.
+            `property_name`: the name of the property that would be inserted.
+            `property_json`: the JSON object that would be parsed to create
+            the new table column property.
+
+        Raises:
+            `PyDoughMetadataException`: if the JSON for the property is
+            malformed.
+        """
+        error_name: str = MaskedTableColumnMetadata.create_error_name(
+            property_name, collection.error_name
+        )
+        # Extract the `data_type` and `column_name` fields from the JSON object
+        type_string: str = extract_string(property_json, "data type", error_name)
+        try:
+            data_type: PyDoughType = parse_type_from_string(type_string)
+        except PyDoughTypeException as e:
+            raise PyDoughMetadataException(*e.args)
+        column_name: str = extract_string(property_json, "column name", error_name)
+
+        # Extract the `unprotect protocol`, `protect protocol`, and
+        # `server masked` fields from the JSON object.
+        unprotect_protocol: str = extract_string(
+            property_json, "unprotect protocol", error_name
+        )
+        protect_protocol: str = extract_string(
+            property_json, "protect protocol", error_name
+        )
+        server_masked: bool = False
+        if "server masked" in property_json:
+            server_masked = extract_bool(property_json, "server masked", error_name)
+
+        NoExtraKeys(MaskedTableColumnMetadata.allowed_fields).verify(
+            property_json, error_name
+        )
+
+        # Extract the optional fields from the JSON object.
+        sample_values: list | None = None
+        description: str | None = None
+        synonyms: list[str] | None = None
+        extra_semantic_info: dict | None = None
+        if "sample values" in property_json:
+            sample_values = extract_array(property_json, "sample values", error_name)
+        if "description" in property_json:
+            description = extract_string(property_json, "description", error_name)
+        if "synonyms" in property_json:
+            synonyms = extract_array(property_json, "synonyms", error_name)
+        if "extra semantic info" in property_json:
+            extra_semantic_info = extract_object(
+                property_json, "extra semantic info", error_name
+            )
+
+        # Build the new property metadata object and add it to the collection.
+        property: MaskedTableColumnMetadata = MaskedTableColumnMetadata(
+            property_name,
+            collection,
+            data_type,
+            column_name,
+            unprotect_protocol,
+            protect_protocol,
+            server_masked,
+            sample_values,
+            description,
+            synonyms,
+            extra_semantic_info,
+        )
+        collection.add_property(property)
