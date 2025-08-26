66a19e911fb9268af24651102da5cacbf0c9c67a
knassre-bodo <kian@bodo.ai>
2025-08-26 16:18:36 -0400

    Add logic for simplifying literals that are masked

--- pydough/conversion/masking_shuttles.py
+++ pydough/conversion/masking_shuttles.py
@@ -4,6 +4,9 @@ TODO
 
 __all__ = ["MaskLiteralComparisonShuttle"]
 
+from sqlglot import expressions as sqlglot_expressions
+from sqlglot import parse_one
+
 import pydough.pydough_operators as pydop
 from pydough.relational import (
     CallExpression,
@@ -11,6 +14,9 @@ from pydough.relational import (
     RelationalExpression,
     RelationalExpressionShuttle,
 )
+from pydough.sqlglot import convert_sqlglot_to_relational
+
+from .relational_simplification import SimplificationShuttle
 
 
 class MaskLiteralComparisonShuttle(RelationalExpressionShuttle):
@@ -18,15 +24,38 @@ class MaskLiteralComparisonShuttle(RelationalExpressionShuttle):
     TODO
     """
 
-    def is_unprotect_call(self, expr: RelationalExpression) -> bool:
+    def __init__(self):
+        self.simplifier: SimplificationShuttle = SimplificationShuttle()
+
+    def simplify_masked_literal(
+        self, value: RelationalExpression
+    ) -> RelationalExpression:
         """
         TODO
         """
-        return (
-            isinstance(expr, CallExpression)
-            and isinstance(expr.op, pydop.MaskedExpressionFunctionOperator)
-            and expr.op.is_unprotect
-        )
+        if (
+            not isinstance(value, CallExpression)
+            or not isinstance(value.op, pydop.MaskedExpressionFunctionOperator)
+            or value.op.is_unprotect
+            or len(value.inputs) != 1
+            or not isinstance(value.inputs[0], LiteralExpression)
+        ):
+            return value
+        try:
+            arg_sql_str: str = sqlglot_expressions.convert(value.inputs[0].value).sql()
+            total_sql_str: str = value.op.format_string.format(arg_sql_str)
+            glot_expr: sqlglot_expressions.Expression = parse_one(total_sql_str)
+            new_expr: RelationalExpression | None = convert_sqlglot_to_relational(
+                glot_expr
+            )
+            if new_expr is not None:
+                return new_expr
+            self.simplifier.reset()
+            return value.accept_shuttle(self.simplifier)
+        except Exception:
+            return value
+
+        return value
 
     def protect_literal_comparison(
         self,
@@ -53,22 +82,23 @@ class MaskLiteralComparisonShuttle(RelationalExpressionShuttle):
                 call_arg.data_type,
                 [literal_arg],
             )
+            masked_literal = self.simplify_masked_literal(masked_literal)
         elif original_call.op == pydop.ISIN and isinstance(
             literal_arg.value, (list, tuple)
         ):
-            masked_literal = LiteralExpression(
-                [
-                    CallExpression(
-                        pydop.MaskedExpressionFunctionOperator(
-                            call_arg.op.masking_metadata, False
-                        ),
-                        call_arg.data_type,
-                        [LiteralExpression(v, literal_arg.data_type)],
-                    )
-                    for v in literal_arg.value
-                ],
-                original_call.data_type,
-            )
+            new_elems: list[RelationalExpression] = []
+            for val in literal_arg.value:
+                new_val: RelationalExpression = CallExpression(
+                    pydop.MaskedExpressionFunctionOperator(
+                        call_arg.op.masking_metadata, False
+                    ),
+                    call_arg.data_type,
+                    [LiteralExpression(val, literal_arg.data_type)],
+                )
+                new_val = self.simplify_masked_literal(new_val)
+                new_elems.append(new_val)
+
+            masked_literal = LiteralExpression(new_elems, original_call.data_type)
         else:
             return original_call
 
@@ -82,6 +112,8 @@ class MaskLiteralComparisonShuttle(RelationalExpressionShuttle):
         self, call_expression: CallExpression
     ) -> RelationalExpression:
         if call_expression.op in (pydop.EQU, pydop.NEQ):
+            # UNMASK(expr) = literal  -->  expr = MASK(literal)
+            # UNMASK(expr) != literal  -->  expr != MASK(literal)
             if isinstance(call_expression.inputs[0], CallExpression) and isinstance(
                 call_expression.inputs[1], LiteralExpression
             ):
@@ -90,6 +122,8 @@ class MaskLiteralComparisonShuttle(RelationalExpressionShuttle):
                     call_expression.inputs[0],
                     call_expression.inputs[1],
                 )
+            # literal = UNMASK(expr)  -->  MASK(literal) = expr
+            # literal != UNMASK(expr)  -->  MASK(literal) != expr
             if isinstance(call_expression.inputs[1], CallExpression) and isinstance(
                 call_expression.inputs[0], LiteralExpression
             ):
@@ -98,6 +132,7 @@ class MaskLiteralComparisonShuttle(RelationalExpressionShuttle):
                     call_expression.inputs[1],
                     call_expression.inputs[0],
                 )
+        # UNMASK(expr) IN (x, y, z)  -->  expr IN (MASK(x), MASK(y), MASK(z))
         if (
             call_expression.op == pydop.ISIN
             and isinstance(call_expression.inputs[0], CallExpression)
--- pydough/sqlglot/__init__.py
+++ pydough/sqlglot/__init__.py
@@ -3,6 +3,7 @@ __all__ = [
     "SQLGlotRelationalVisitor",
     "convert_dialect_to_sqlglot",
     "convert_relation_to_sql",
+    "convert_sqlglot_to_relational",
     "execute_df",
     "find_identifiers",
     "find_identifiers_in_list",
@@ -19,3 +20,4 @@ from .sqlglot_helpers import get_glot_name, set_glot_alias, unwrap_alias
 from .sqlglot_identifier_finder import find_identifiers, find_identifiers_in_list
 from .sqlglot_relational_expression_visitor import SQLGlotRelationalExpressionVisitor
 from .sqlglot_relational_visitor import SQLGlotRelationalVisitor
+from .sqlglot_to_relational import convert_sqlglot_to_relational
