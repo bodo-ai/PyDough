712d04587baa45e6485f6cb582085ee16cbca44f
knassre-bodo <kian@bodo.ai>
2025-10-17 22:41:34 -0400

    Minor updates to the defog graph

--- tests/test_sql_refsols/defog_dealership_adv12_postgres.sql
+++ tests/test_sql_refsols/defog_dealership_adv12_postgres.sql
@@ -1,28 +1,14 @@
-WITH _t1 AS (
-  SELECT
-    cars._id AS _id_1,
-    MAX(sales.car_id) AS anything_car_id,
-    MAX(cars.make) AS anything_make,
-    MAX(cars.model) AS anything_model,
-    MAX(sales.sale_price) AS anything_sale_price
-  FROM main.sales AS sales
-  JOIN main.cars AS cars
-    ON cars._id = sales.car_id
-  JOIN main.inventory_snapshots AS inventory_snapshots
-    ON NOT inventory_snapshots.is_in_inventory
-    AND cars._id = inventory_snapshots.car_id
-    AND inventory_snapshots.snapshot_date = sales.sale_date
-  GROUP BY
-    sales._id,
-    1
-)
 SELECT
-  anything_make AS make,
-  anything_model AS model,
-  anything_sale_price AS sale_price
-FROM _t1
-WHERE
-  _id_1 = anything_car_id
+  cars.make,
+  cars.model,
+  sales.sale_price
+FROM main.sales AS sales
+JOIN main.cars AS cars
+  ON cars._id = sales.car_id
+JOIN main.inventory_snapshots AS inventory_snapshots
+  ON NOT inventory_snapshots.is_in_inventory
+  AND cars._id = inventory_snapshots.car_id
+  AND inventory_snapshots.snapshot_date = sales.sale_date
 ORDER BY
   3 DESC NULLS LAST
 LIMIT 1
