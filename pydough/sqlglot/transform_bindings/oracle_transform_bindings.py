69478a6a30da643893c5e0df056e503415efad92
john-sanchez31 <johnsanchez@bodo.ai>
2026-02-06 09:35:47 -0600

    WIP: slice and get_part

--- pydough/sqlglot/transform_bindings/oracle_transform_bindings.py
+++ pydough/sqlglot/transform_bindings/oracle_transform_bindings.py
@@ -26,7 +26,6 @@ class OracleTransformBindings(BaseTransformBindings):
         pydop.SMALLEST: "LEAST",
         pydop.STRIP: "TRIM",
         pydop.FIND: "INSTR",
-        pydop.SLICE: "SUBSTR",
         pydop.JOIN_STRINGS: "LISTAGG",
         pydop.PERCENTILE: "PERCENTILE_CONT",
     }
@@ -46,5 +45,263 @@ class OracleTransformBindings(BaseTransformBindings):
             return sqlglot_expressions.Anonymous(
                 this=self.PYDOP_TO_ORACLE_FUNC[operator], expressions=args
             )
-
         return super().convert_call_to_sqlglot(operator, args, types)
+
+    def convert_slice(
+        self, args: list[SQLGlotExpression], types: list[PyDoughType]
+    ) -> SQLGlotExpression:
+        """
+        Convert a PyDough slice operation to a SQLGlot expression.
+        MySQL uses the SUBSTRING function for slicing.
+
+        Outline of the logic:
+        - If the start index is None, it defaults to 1 (1-based indexing).
+        - If the stop index is None, it defaults to the length of the string.
+        - a = start index
+        - b = stop index
+        match (a, b):
+            case (None, None):
+                return SUBSTRING(x, 1)
+            case (+a, None):
+                return SUBSTRING(x, a + 1)
+            case (-a, None):
+                return SUBSTRING(x, a)
+            case (None, +b):
+                return SUBSTRING(x, 1, b)
+            case (None, -b):
+                return SUBSTRING(x, 1, LENGTH(x) + b)
+            case (+a, +b):
+                return SUBSTRING(x, a + 1, GREATEST(b - a, 0))
+            case (-a, -b):
+                return SUBSTRING(x, a, GREATEST(b - a, 0))
+            case (+a, -b):
+                return SUBSTRING(x, a + 1, GREATEST(LENGTH(x) + b - a, 0))
+            case (-a, +b):
+                return SUBSTRING(x, a, b - GREATEST(LENGTH(x) + a, 0))
+        """
+
+        assert len(args) == 4
+        string_expr, start, stop, step = args
+
+        start_idx: int | None = None
+        if not isinstance(start, sqlglot_expressions.Null):
+            if isinstance(start, sqlglot_expressions.Literal):
+                try:
+                    start_idx = int(start.this)
+                except ValueError:
+                    raise ValueError(
+                        "SLICE function currently only supports the start index being integer literal or absent."
+                    )
+            else:
+                raise ValueError(
+                    "SLICE function currently only supports the start index being integer literal or absent."
+                )
+
+        stop_idx: int | None = None
+        if not isinstance(stop, sqlglot_expressions.Null):
+            if isinstance(stop, sqlglot_expressions.Literal):
+                try:
+                    stop_idx = int(stop.this)
+                except ValueError:
+                    raise ValueError(
+                        "SLICE function currently only supports the stop index being integer literal or absent."
+                    )
+            else:
+                raise ValueError(
+                    "SLICE function currently only supports the stop index being integer literal or absent."
+                )
+
+        step_idx: int | None = None
+        if not isinstance(step, sqlglot_expressions.Null):
+            if isinstance(step, sqlglot_expressions.Literal):
+                try:
+                    step_idx = int(step.this)
+                    if step_idx != 1:
+                        raise ValueError(
+                            "SLICE function currently only supports the step being integer literal 1 or absent."
+                        )
+                except ValueError:
+                    raise ValueError(
+                        "SLICE function currently only supports the step being integer literal 1 or absent."
+                    )
+            else:
+                raise ValueError(
+                    "SLICE function currently only supports the step being integer literal 1 or absent."
+                )
+
+        # SQLGlot expressions for 0 and 1 and empty string
+        sql_one: SQLGlotExpression = sqlglot_expressions.Literal.number(1)
+        sql_zero: SQLGlotExpression = sqlglot_expressions.Literal.number(0)
+        expr_length: SQLGlotExpression = sqlglot_expressions.Length(this=string_expr)
+        one_index_start: SQLGlotExpression = sqlglot_expressions.Add(
+            this=start, expression=sql_one
+        )
+        # length adjustment
+        length: SQLGlotExpression = None
+
+        match (start_idx, stop_idx):
+            case (None, end_idx) if end_idx is not None and end_idx >= 0:
+                length = stop
+
+            case (None, end_idx) if end_idx is not None and end_idx < 0:
+                length = sqlglot_expressions.Add(this=expr_length, expression=stop)
+
+            case (begin_idx, end_idx) if (
+                begin_idx is not None
+                and end_idx is not None
+                and begin_idx >= 0
+                and end_idx >= 0
+            ):
+                length = sqlglot_expressions.Greatest(
+                    this=sqlglot_expressions.Sub(this=stop, expression=start),
+                    expressions=[sql_zero],
+                )
+
+            case (begin_idx, end_idx) if (
+                begin_idx is not None
+                and end_idx is not None
+                and begin_idx < 0
+                and end_idx < 0
+            ):
+                length = sqlglot_expressions.Case(
+                    ifs=[
+                        sqlglot_expressions.If(
+                            this=sqlglot_expressions.LT(
+                                this=expr_length,
+                                expression=sqlglot_expressions.Abs(this=start),
+                            ),
+                            true=sqlglot_expressions.Add(
+                                this=expr_length, expression=stop
+                            ),
+                        )
+                    ],
+                    default=sqlglot_expressions.Greatest(
+                        this=sqlglot_expressions.Sub(this=stop, expression=start),
+                        expressions=[sql_zero],
+                    ),
+                )
+
+            case (begin_idx, end_idx) if (
+                begin_idx is not None
+                and end_idx is not None
+                and begin_idx >= 0
+                and end_idx < 0
+            ):
+                length = sqlglot_expressions.Greatest(
+                    this=sqlglot_expressions.Sub(
+                        this=sqlglot_expressions.Add(this=expr_length, expression=stop),
+                        expression=start,
+                    ),
+                    expressions=[sql_zero],
+                )
+
+            case (begin_idx, end_idx) if (
+                begin_idx is not None
+                and end_idx is not None
+                and begin_idx < 0
+                and end_idx >= 0
+            ):
+                length = sqlglot_expressions.Sub(
+                    this=stop,
+                    expression=sqlglot_expressions.Greatest(
+                        this=sqlglot_expressions.Add(
+                            this=expr_length, expression=start
+                        ),
+                        expressions=[sql_zero],
+                    ),
+                )
+
+        # start adjustment
+        if start_idx is not None and start_idx >= 0:
+            start = one_index_start
+        elif start_idx is not None and start_idx < 0:
+            start = sqlglot_expressions.Case(
+                ifs=[
+                    sqlglot_expressions.If(
+                        this=sqlglot_expressions.GT(
+                            this=expr_length,
+                            expression=sqlglot_expressions.Abs(this=start),
+                        ),
+                        true=start,
+                    )
+                ],
+                default=sql_one,
+            )
+
+        elif start_idx is None:
+            start = sql_one
+
+        result: SQLGlotExpression = sqlglot_expressions.Substring(
+            this=string_expr, start=start, length=length
+        )
+        return result
+
+    def convert_get_part(
+        self, args: list[SQLGlotExpression], types: list[PyDoughType]
+    ) -> SQLGlotExpression:
+        """
+        GETPART(str, delim, idx) ->
+        REGEXP_SUBSTR(
+            str,
+            '[^' || delim || ']+',
+            1,
+            CASE
+                WHEN idx = 0 THEN 1
+                WHEN idx > 0 THEN idx
+                ELSE (REGEXP_COUNT(str, delim) + 1) + idx + 1
+            END
+        )
+        """
+
+        assert len(args) == 3
+
+        string_expr, delimiter_expr, index_expr = args
+        literal_0: SQLGlotExpression = sqlglot_expressions.Literal.number(0)
+        literal_1: SQLGlotExpression = sqlglot_expressions.Literal.number(1)
+        regex_1: SQLGlotExpression = sqlglot_expressions.Literal.string("[^")
+        regex_2: SQLGlotExpression = sqlglot_expressions.Literal.string("]+")
+
+        case_expr: SQLGlotExpression = sqlglot_expressions.Case(
+            ifs=[
+                sqlglot_expressions.If(
+                    this=sqlglot_expressions.EQ(this=index_expr, expression=literal_0),
+                    true=literal_1,
+                ),
+                sqlglot_expressions.If(
+                    this=sqlglot_expressions.GT(this=index_expr, expression=literal_0),
+                    true=index_expr,
+                ),
+            ],
+            default=sqlglot_expressions.Add(
+                this=sqlglot_expressions.Add(
+                    this=sqlglot_expressions.Paren(
+                        this=sqlglot_expressions.Add(
+                            this=sqlglot_expressions.Anonymous(
+                                this="REGEXP_COUNT",
+                                expressions=[string_expr, delimiter_expr],
+                            ),
+                            expression=literal_1,
+                        )
+                    ),
+                    expression=index_expr,
+                ),
+                expression=literal_1,
+            ),
+        )
+
+        result: SQLGlotExpression = sqlglot_expressions.Anonymous(
+            this="REGEXP_SUBSTR",
+            expressions=[
+                string_expr,
+                sqlglot_expressions.DPipe(
+                    this=sqlglot_expressions.DPipe(
+                        this=regex_1, expression=delimiter_expr, safe=True
+                    ),
+                    expression=regex_2,
+                    safe=True,
+                ),
+                literal_1,
+                case_expr,
+            ],
+        )
+        return result
