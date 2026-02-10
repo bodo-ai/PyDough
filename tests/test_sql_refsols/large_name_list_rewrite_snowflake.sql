15a217e99d6bca5e35f09f57be7d8dd92fdfadc0
knassre-bodo <105652923+knassre-bodo@users.noreply.github.com>
2026-02-10 00:02:01 -0800

    Adding some normalization to relational expressions (#487)
    For both visual clarity and ease of writing optimizations, the following are all now automatically done in relational expressions:
    * Flatten all AND/OR expressions
    * Sort the inputs to AND, OR, EQU and NEQ so that `(A==B)` is always the same as `(B==A)`
    * Place parentheses around binary expressions inside other binary expressions for ease of reading

--- tests/test_sql_refsols/large_name_list_rewrite_snowflake.sql
+++ tests/test_sql_refsols/large_name_list_rewrite_snowflake.sql
@@ -2,7 +2,15 @@ SELECT
   COUNT(*) AS n
 FROM bodo.health.protected_patients
 WHERE
-  last_name IN ('CJlhbU', 'vAPXb', 'qYyk', 'tMwku', 'dIGJWkI', 'YoNIDCH', 'CAyhjdZ', 'MLKXoS', 'zzgJxp', 'AgZiGWu', 'jxHGuRHH', 'euDVUPsl', 'sQRoyTXq', 'LBLElfal', 'FByIFg', 'NCimpJXE', 'fQPcn', 'DoMLPqo', 'KUFOmQ', 'oyStpJKSe', 'CUut', 'egusDkV', 'IQyfxm', 'vZugS', 'uwuArWBU', 'nIfzuW', 'CKyvLUE', 'qfnLDcsrg', 'SmHQQ', 'xhbltD', 'TDv', 'lFtpu', 'CCgefFH', 'bWWubm', 'NoMGLQ', 'pjLmBOK', 'gvODbKmJ', 'DuYWa', 'JFHk', 'lcXG', 'POUZ', 'SrkGsRg', 'NXrRCe', 'gCmWVt', 'DNlnj', 'mkpeex', 'HkCbvqTs')
+  last_name IN ('CAyhjdZ', 'MLKXoS', 'zzgJxp', 'AgZiGWu', 'jxHGuRHH')
+  OR last_name IN ('CJlhbU', 'vAPXb', 'qYyk', 'tMwku', 'dIGJWkI', 'YoNIDCH')
+  OR last_name IN ('CKyvLUE', 'qfnLDcsrg', 'SmHQQ', 'xhbltD', 'TDv')
+  OR last_name IN ('NXrRCe', 'gCmWVt', 'DNlnj', 'mkpeex', 'HkCbvqTs')
   OR last_name IN ('VTFJ', 'yGRL', 'vscqPe', 'uBYHOH')
+  OR last_name IN ('egusDkV', 'IQyfxm', 'vZugS', 'uwuArWBU', 'nIfzuW')
+  OR last_name IN ('euDVUPsl', 'sQRoyTXq', 'LBLElfal', 'FByIFg', 'NCimpJXE')
+  OR last_name IN ('fQPcn', 'DoMLPqo', 'KUFOmQ', 'oyStpJKSe', 'CUut')
+  OR last_name IN ('gvODbKmJ', 'DuYWa', 'JFHk', 'lcXG', 'POUZ', 'SrkGsRg')
+  OR last_name IN ('lFtpu', 'CCgefFH', 'bWWubm', 'NoMGLQ', 'pjLmBOK')
   OR last_name IN ('vrsxxF', 'NlEgBxNp', 'zBrqYCN', 'NVfXDc', 'CVrcz')
   OR last_name IN ('zoaXspT', 'ESotCk', 'ImEdKgbM', 'PBKsp')
