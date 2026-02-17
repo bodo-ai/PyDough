fcbc4faf0be46a4e8272638a0e622f438cfdbba9
knassre-bodo <kian@bodo.ai>
2026-02-17 10:45:11 -0800

    Resolving conflicts

--- tests/test_sql_refsols/color_q13_snowflake.sql
+++ tests/test_sql_refsols/color_q13_snowflake.sql
@@ -1,42 +1,13 @@
-WITH _s0 AS (
-  SELECT
-    COUNT(*) AS n_rows
-  FROM shpmnts
-  WHERE
-    colid = 'avocado'
-), _s1 AS (
-  SELECT
-    COUNT(*) AS n_rows
-  FROM shpmnts
-  WHERE
-    colid = 'blue_bell'
-), _s3 AS (
-  SELECT
-    COUNT(*) AS n_rows
-  FROM shpmnts
-  WHERE
-    colid = 'carmine'
-), _s5 AS (
-  SELECT
-    COUNT(*) AS n_rows
-  FROM shpmnts
-  WHERE
-    colid = 'deep_fuchsia'
-), _s7 AS (
-  SELECT
-    COUNT(*) AS n_rows
-  FROM shpmnts
-  WHERE
-    colid = 'mode_beige'
-)
 SELECT
-  _s0.n_rows AS n_avocado,
-  _s1.n_rows AS n_blue_bell,
-  _s3.n_rows AS n_carmine,
-  _s5.n_rows AS n_deep_fuchsia,
-  _s7.n_rows AS n_mode_beige
-FROM _s0 AS _s0
-CROSS JOIN _s1 AS _s1
-CROSS JOIN _s3 AS _s3
-CROSS JOIN _s5 AS _s5
-CROSS JOIN _s7 AS _s7
+  COUNT_IF(colid = 'avocado') AS n_avocado,
+  COUNT_IF(colid = 'blue_bell') AS n_blue_bell,
+  COUNT_IF(colid = 'carmine') AS n_carmine,
+  COUNT_IF(colid = 'deep_fuchsia') AS n_deep_fuchsia,
+  COUNT_IF(colid = 'mode_beige') AS n_mode_beige
+FROM shpmnts
+WHERE
+  colid = 'avocado'
+  OR colid = 'blue_bell'
+  OR colid = 'carmine'
+  OR colid = 'deep_fuchsia'
+  OR colid = 'mode_beige'
