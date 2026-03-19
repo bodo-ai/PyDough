2e20048437cf00b1245b9b8b10d3d96d6a51bba7
john-sanchez31 <johnsanchez@bodo.ai>
2026-03-19 09:27:05 -0600

    adding fixture [run ci][run dialects]

--- tests/conftest.py
+++ tests/conftest.py
@@ -101,6 +101,7 @@ from tests.testing_utilities import PyDoughPandasTest, graph_fetcher
 
 from .gen_data.gen_pagerank import gen_pagerank_records, pagerank_configs
 from .gen_data.gen_technograph import gen_technograph_records
+from .test_pipeline_bodosql import bodosql_tpch_context  # noqa
 
 
 @pytest.fixture
